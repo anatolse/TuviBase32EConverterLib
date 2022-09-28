a8a6f512392de2fb74f13bb7b1ce991b83acbc47
Anatoly <gymbotan@rambler.ru>
2022-09-28 12:14:48 +0300

    Feat: Add TuviConverterLid and TuviConverterTests

---
+++ TuviBytesToEmailConverterLib/TuviBytesToEmailConverterLib/TuviConverter.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Numerics;
+
+namespace TuviBytesToEmailConverterLib
+{
+    public class TuviConverter
+    {
+        public string ConvertBytesToEmailName(byte[] array)
+        {
+            if (array is null)
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
+
+            byte[] fiveBitsArray = ConvertEightToFiveBits(array);
+            char[] symbolsArray = new char[fiveBitsArray.Length];
+            for (int i = 0; i < symbolsArray.Length; i++)
+            {
+                symbolsArray[i] = ConvertByteToSymbol(fiveBitsArray[i]);
+            }
+            return new string(symbolsArray);
+        }
+
+        public byte[] ConvertStringToByteArray(string name)
+        {
+            if (name is null)
+            {
+                throw new ArgumentNullException(nameof(name));
+            }
+
+            if (name.Length > 64)
+            {
+                throw new ArgumentException("Email's name can not be longer than 64 symbols.", nameof(name));
+            }
+
+            char[] symbolsArray = name.ToCharArray();
+            byte[] fiveBitsArray = new byte[symbolsArray.Length];
+
+            for (int i = 0; i < fiveBitsArray.Length; i++)
+            {
+                fiveBitsArray[i] = ToBits(symbolsArray[i]);
+            }
+
+            return ConvertFiveToEightBits(fiveBitsArray);
+        }
+
+        private byte[] ConvertEightToFiveBits(byte[] array)
+        {
+            if (array is null)
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
+
+            int size = array.Length * 8 % 5 == 0 ? array.Length * 8 / 5 : array.Length * 8 / 5 + 1;
+            int currentPosition = size - 1;
+            byte[] result = new byte[size];
+            BigInteger number = new BigInteger(array, true, true);
+            while (number != 0 && currentPosition >= 0)
+            {
+                byte lastFiveBits = (byte)(number & 31);
+                result[currentPosition] = lastFiveBits;
+                number = number >> 5;
+                currentPosition--;
+            }
+
+            if (result.Length > 64)
+            {
+                throw new ArgumentException("Initial array is too big to create coorect email's name (name can not be longer than 64 symbols).", nameof(array));
+            }
+
+            return result;
+        }
+
+        private byte[] ConvertFiveToEightBits(byte[] array)
+        {
+            if (array is null)
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
+
+            if (array.Length > 64)
+            {
+                throw new ArgumentException("Email's name can not be longer than 64 symbols.", nameof(array));
+            }
+
+            BigInteger number = 0;
+            int size = array.Length * 5 / 8;
+            byte[] resultArray = new byte[size];
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] >= 32)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(array),
+                        $"Array at index {i} has wrong value. Allowed values are from 0 to 31.");
+                }
+                number = number << 5;
+                number |= array[i];
+            }
+
+            byte[] tempArray = number.ToByteArray(true, true);
+            if (tempArray.Length >= resultArray.Length)
+            {
+                return tempArray;
+            }
+            else
+            {
+                for (int i = 1; i <= tempArray.Length; i++)
+                {
+                    resultArray[^i] = tempArray[^i];
+                }
+
+                return resultArray;
+            }
+        }
+
+        private char ConvertByteToSymbol(byte bits) => bits switch
+        {
+            0 => 'a',
+            1 => 'b',
+            2 => 'c',
+            3 => 'd',
+            4 => 'e',
+            5 => 'f',
+            6 => 'g',
+            7 => 'h',
+            8 => 'i',
+            9 => 'j',
+            10 => 'k',
+            11 => 'l',
+            12 => 'm',
+            13 => 'n',
+            14 => 'o',
+            15 => 'p',
+            16 => 'q',
+            17 => 'r',
+            18 => 's',
+            19 => 't',
+            20 => 'u',
+            21 => 'v',
+            22 => 'w',
+            23 => 'x',
+            24 => 'y',
+            25 => 'z',
+            26 => '2',
+            27 => '3',
+            28 => '4',
+            29 => '5',
+            30 => '6',
+            31 => '7',
+            _ => throw new ArgumentOutOfRangeException(nameof(bits)),
+        };
+
+        private byte ToBits(char symbol) => symbol switch
+        {
+            'a' => 0,
+            'b' => 1,
+            'c' => 2,
+            'd' => 3,
+            'e' => 4,
+            'f' => 5,
+            'g' => 6,
+            'h' => 7,
+            'i' => 8,
+            'j' => 9,
+            'k' => 10,
+            'l' => 11,
+            'm' => 12,
+            'n' => 13,
+            'o' => 14,
+            'p' => 15,
+            'q' => 16,
+            'r' => 17,
+            's' => 18,
+            't' => 19,
+            'u' => 20,
+            'v' => 21,
+            'w' => 22,
+            'x' => 23,
+            'y' => 24,
+            'z' => 25,
+            '2' => 26,
+            '3' => 27,
+            '4' => 28,
+            '5' => 29,
+            '6' => 30,
+            '7' => 31,
+            _ => throw new ArgumentOutOfRangeException(nameof(symbol)),
+        };
+
+    }
+}
---
+++ TuviBytesToEmailConverterLib/TuviConverterTests/TuviConverterTests.cs
@@ -0,0 +1,118 @@
+using NUnit.Framework;
+using System;
+using TuviBytesToEmailConverterLib;
+
+namespace TuviConverterTests
+{
+    public class TuviConverterTests
+    {
+        [TestCase(new byte[] { 0, 0, 1 }, "aaaab")]
+        [TestCase(new byte[] { 31, 32, 33 }, "b6ibb")]
+        [TestCase(new byte[] { 233, 74, 89, 152, 12 }, "5ffftgam")]
+        [TestCase(new byte[] { 111, 29, 94, 201, 215, 75 }, "dpdvpmtv2l")]
+        [TestCase(new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33 }, "acaqdaqcqmbyibefawdanbyhraeiscmkbkfqxdamrugy4dupb6ibb")]
+        public void BytesToString_CorrectConverting(byte[] array, string expectedResult)
+        {
+            TuviConverter converter = new TuviConverter();
+            var actualResult = converter.ConvertBytesToEmailName(array);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestCase(new byte[] { 0, 0, 1 })]
+        [TestCase(new byte[] { 255, 255, 255 })]
+        [TestCase(new byte[] { 1, 1, 1 })]
+        [TestCase(new byte[] { 128, 255, 1 })]
+        [TestCase(new byte[] { 18, 231, 112 })]
+        [TestCase(new byte[] { 0, 0, 0, 1 })]
+        [TestCase(new byte[] { 255, 255, 255, 255 })]
+        [TestCase(new byte[] { 1, 1, 1, 1 })]
+        [TestCase(new byte[] { 138, 31, 192, 211, 234, 7, 77, 177, 54, 139 })]
+        public void BytesToStringAndToBytes_CorrectConverting(byte[] array)
+        {
+            TuviConverter converter = new TuviConverter();
+            var actualResult = converter.ConvertStringToByteArray(converter.ConvertBytesToEmailName(array));
+            Assert.AreEqual(array, actualResult);
+        }
+
+        [TestCase("aaah7", new byte[] { 0, 0, 255 })]
+        [TestCase("b6ibb", new byte[] { 31, 32, 33 })]
+        [TestCase("friend", new byte[] { 11, 20, 17, 163 })]
+        [TestCase("maroon5", new byte[] { 3, 1, 23, 57, 189 })]
+        [TestCase("hfbvmtj46djg7xvgdxxmoloe35fgjnmnfd73khj5fxfb2egloj2bx", new byte[] { 114, 134, 172, 154, 121, 225, 164, 223, 189, 76, 59, 221, 142, 91, 137, 190, 148, 201, 107, 26, 81, 255, 106, 58, 122, 91, 148, 58, 33, 150, 228, 232, 55 })]
+        public void StringToBytes_CorrectConverting(string name, byte[] expectedResult)
+        {
+            TuviConverter converter = new TuviConverter();
+            var actualResult = converter.ConvertStringToByteArray(name);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestCase("abc0")]
+        [TestCase("abc1")]
+        [TestCase("abc8")]
+        [TestCase("abc9")]
+        [TestCase("abc.")]
+        [TestCase("abc,")]
+        [TestCase("abc?")]
+        [TestCase("abc/")]
+        [TestCase("abc\\")]
+        [TestCase("abc;")]
+        [TestCase("abc:")]
+        [TestCase("abc'")]
+        [TestCase("abc\"")]
+        [TestCase("abc!")]
+        [TestCase("abc@")]
+        [TestCase("abc#")]
+        [TestCase("abc$")]
+        [TestCase("abc%")]
+        [TestCase("abc^")]
+        [TestCase("abc&")]
+        [TestCase("abc*")]
+        [TestCase("abc(")]
+        [TestCase("abc)")]
+        [TestCase("abc+")]
+        [TestCase("abc-")]
+        [TestCase("abc_")]
+        [TestCase("abc<")]
+        [TestCase("abc>")]
+        public void StringToBytes_WrongSymbols_ThrowArgumentOutOfRangeException(string name)
+        {
+            TuviConverter converter = new TuviConverter();
+            Assert.Throws<ArgumentOutOfRangeException>(() => converter.ConvertStringToByteArray(name),
+                message: "Not allowed symbol are used in email's name.");
+        }
+
+        [TestCase("abcdefjhijklmnopqrstuvwxyz23434567abcdefjhijklmnopqrstuvwxyz23434567a")]
+        [TestCase("fhsadjfhbdsfbajhfhwejghdbcnbdsjkghdjgdfg34hgvdjdjshgajhwsdgahfvjsfgkawuf")]
+        public void StringToBytes_TooBigString_ThrowArgumentException(string name)
+        {
+            TuviConverter converter = new TuviConverter();
+            Assert.Throws<ArgumentException>(() => converter.ConvertStringToByteArray(name),
+                message: "Email's name is too big (name can not be longer than 64 symbols).");
+        }
+
+        [Test]
+        public void StringToBytes_StringIsNull_ThrowArgumentNullException()
+        {
+            TuviConverter converter = new TuviConverter();
+            Assert.Throws<ArgumentNullException>(() => converter.ConvertStringToByteArray(null),
+                message: "Email's name can not be a null.");
+        }
+
+        [Test]
+        public void BytesToString_ArrayIsNull_ThrowArgumentNullException()
+        {
+            TuviConverter converter = new TuviConverter();
+            Assert.Throws<ArgumentNullException>(() => converter.ConvertBytesToEmailName(null),
+                message: "Byte's array can not be a null.");
+        }
+
+        [TestCase(new byte[] { 114, 134, 172, 154, 121, 225, 164, 223, 189, 76, 59, 221, 142, 91, 137, 190, 148, 201, 107, 26, 81, 255, 106, 58, 122, 91, 148, 58, 33, 150, 228, 232, 55, 77, 34, 127, 198, 4, 15, 251, 9, 66, 112, 183, 71 })]
+        [TestCase(new byte[] { 55, 18, 194, 211, 6, 19, 168, 162, 43, 207, 12, 59, 114, 134, 172, 154, 121, 225, 164, 223, 189, 76, 59, 221, 142, 91, 137, 190, 148, 201, 107, 26, 81, 255, 106, 58, 122, 91, 148, 58, 33, 150, 228, 232, 55 })]
+        public void BytesToString_TooBigArray_ThrowArgumentException(byte[] array)
+        {
+            TuviConverter converter = new TuviConverter();
+            Assert.Throws<ArgumentException>(() => converter.ConvertBytesToEmailName(array),
+                message: "Initial array is too big (name can not be longer than 64 symbols).");
+        }
+    }
+}
